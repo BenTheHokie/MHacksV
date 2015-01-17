f61b05589887ab9b5e5f55a1cb941e310a7569ce
Justin Lee <justinlee1995@gmail.com>
2015-01-17 08:55:26 -0500

    Display friend list

--- static/app.js
+++ static/app.js
@@ -1,11 +1,17 @@
 $(".login").click(logInUser);
-
 function display_friends() {
 	var friends = user.relation("friendsUsingApp");
-	relation.query().find({
+	friends.query().find({
 		success: function(friends) {
 			//friends is an array of friends
 			// with the app
+		      console.log('hi');
+		     for(var i = 0;i<friends.length;i++){
+		       $('.friendcontainer').append('<li>'+friends[i].get('name')+' <a href="#">Add to Room</a></li>');
+		       console.log(i+friends[i].get('name'));
+		     }
 		}
 	});
 }
+
+ 
