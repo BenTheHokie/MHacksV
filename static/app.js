19a65dcfb96c7cf772f6d6e8b6d0fb1f282fc503
keoliva <keoliva@andrew.cmu.edu>
2015-01-17 14:17:23 -0500

    create_new_room/get_friends
    when a new room is created, the current user creating it, is added to
    its "members" field, and the room is added to the user's "rooms" field,
    and "lastAccessedRoom" field

--- static/app.js
+++ static/app.js
@@ -1,18 +1,46 @@
 $(".login").click(logInUser);
 var all_friends = null;
-function display_friends() {
+//gets all the friends that aren't your roomies yet
+function get_friends() {
+	var curr_room = user.get("lastAccessedRoom")
+	var members = curr_room.relation("members");
 	var friends = user.relation("friendsUsingApp");
-	friends.query().ascending("name").find({
-		success: function(friends) {
-			all_friends = friends;
-			//friends is an array of friends
-			// with the app
-		      console.log('hi');
-		     for(var i = 0;i<friends.length;i++){
-		       $('.friendcontainer').append('<li>'+friends[i].get('name')+' <a href="#">Add to Room</a></li>');
-		       console.log(i+friends[i].get('name'));
-		     }
+	members.query().find({
+		success: function(list) {
+			var ids = [];
+			for (var i = 0; i < list.length; i++) {
+				ids += [list[i].get("fbID")];
+			}
+			friends.query().notContainedIn("fbID", ids);
+			friends.query().ascending("name");
+			friends.query().find({
+				success: function(results) {
+					console.log(results);
+					return results;
+				}
+			});
 		}
+	});
+}
+
+function create_new_room() {
+	var name = $("#room_name").val();
+	console.log(name);
+	var Room = Parse.Object.extend("Room");
+	var room = new Room();
+	room.set("name", name); //set the name of the room
+	room.relation("members").add(user); //add current user
+	room.save(null, {
+		success: function(current_user) {
+			user.relation("rooms").add(room);
+			user.set("lastAccessedRoom", room);
+			user.save();
+			$('a.close-reveal-modal').trigger('click');
+		},
+	error: function(current_user, error) {
+    // Execute any logic that should take place if the save fails.
+    // error is a Parse.Error with an error code and message.
+	}
 	});
 }
 
