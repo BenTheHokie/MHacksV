251ae16478c9d5b0b13d9f97edeeb49bdeb77570
Justin Lee <justinlee1995@gmail.com>
2015-01-17 02:51:51 -0500

    changed port again

--- server.py
+++ server.py
@@ -14,5 +14,6 @@ def index():
 
 if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
-    #port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port = 5000, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    #app.run(debug=True, port = 5000, host='0.0.0.0')
+    app.run()
