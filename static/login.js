628a3c8903fb1720e052262cdacef9f4f84f72d2
Ben <bwengert@vt.edu>
2015-01-17 12:51:08 -0500

    adding cookie login

--- static/login.js
+++ static/login.js
@@ -12,7 +12,9 @@ function onLogin() {
 
             token = user.attributes.authData.facebook.access_token;
 
+	    console.log("Attempting to set cookie...");
 	    $.cookie("accesstoken", token);
+	    window.location.reload();
             // name = user._serverData.name;
             // console.log("User's Name: " + name + "\nAccess Token: " + token + "\nNow we can make a POST request that can be validated by the server, bitches.");
 
