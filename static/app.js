9b47705179464055d929af5c8170df50600f4802
keoliva <keoliva@andrew.cmu.edu>
2015-01-17 08:39:58 -0500

    pulled up all the friendsusingtheapp
    have the array stored in a variable

--- static/app.js
+++ static/app.js
@@ -1,1 +1,11 @@
 $(".login").click(logInUser);
+
+function display_friends() {
+	var friends = user.relation("friendsUsingApp");
+	relation.query().find({
+		success: function(friends) {
+			//friends is an array of friends
+			// with the app
+		}
+	});
+}
