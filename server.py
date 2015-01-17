04a50048e45fcff609bd7cfb259b040a0a93d19e
Justin Lee <justinlee1995@gmail.com>
2015-01-17 02:29:50 -0500

    updated url

--- server.py
+++ server.py
@@ -14,5 +14,5 @@ def index():
 
 if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run()
+    #port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port = 80, host='0.0.0.0')
